634dd5d04eaebcde47ee839245f86c4788b940f2
Jan Costandius <jcostandius@gmail.com>
2024-11-13 11:57:32 +0200

    Fixed enrollment status resource

--- modules/cost/main.tf
+++ modules/cost/main.tf
@@ -1,11 +1,11 @@
-resource "aws_costoptimizationhub_enrollment_status" "enable" {}
+resource "aws_costoptimizationhub_enrollment_status" "enable" {
+  status                  = "Active"
+  include_member_accounts = false
+}
 
 resource "aws_computeoptimizer_enrollment_status" "enable" {
   status                  = "Active"
   include_member_accounts = false
-  lifecycle {
-    ignore_changes = [status]
-  }
 }
 
 resource "aws_resourceexplorer2_index" "local" {

67ecfe1581a0a031de636acf4a8a33ea80598c5d
Jan Costandius <jcostandius@gmail.com>
2024-11-13 11:59:29 +0200

    Added lifecycle block

--- modules/cost/main.tf
+++ modules/cost/main.tf
@@ -1,6 +1,8 @@
 resource "aws_costoptimizationhub_enrollment_status" "enable" {
-  status                  = "Active"
   include_member_accounts = false
+  lifecycle {
+    ignore_changes = [status]
+  }
 }
 
 resource "aws_computeoptimizer_enrollment_status" "enable" {

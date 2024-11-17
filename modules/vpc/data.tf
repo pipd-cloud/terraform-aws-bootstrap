154981f78ffd467cd78e3110082eedcfdeb69f04
Jan Costandius <jcostandius@gmail.com>
2024-11-17 13:13:09 +0200

    Altered AZ

--- modules/vpc/data.tf
+++ modules/vpc/data.tf
@@ -1,6 +1,4 @@
-data "aws_availability_zones" "az" {
-  all_availability_zones = true
-}
+data "aws_availability_zones" "az" {}
 
 data "aws_s3_bucket" "flow_logs_bucket" {
   bucket = var.bucket_name
